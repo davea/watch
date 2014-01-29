6ae1070d71fa624ea5853418f5ee5d43fc3e4f53
Dave Arter <davea@mysociety.org>
2014-01-29 21:39:39 +0000

    Some refactoring to enable multiple browsers to be refreshed simultaneously

--- watch.py
+++ watch.py
@@ -3,8 +3,11 @@
 import sys
 import os
 import time
-from fsevents import Stream, Observer
+from functools import partial
+
 from ScriptingBridge import SBApplication
+
+from fsevents import Stream, Observer
 
 FILE_TYPES = ['css', 'html', 'htm', 'php', 'rb', 'erb', 'less', 'js', 'py']
 
@@ -23,6 +26,10 @@ FLAGS = (
     0x2,    # kFSEventStreamEventFlagUserDropped - seems to be needed too
 )
 
+# The various functions that refresh specific browsers are registered in this dict and called when
+# a watched file changes.
+browser_reloaders = {}
+
 def reload_chrome():
     chrome = SBApplication.applicationWithBundleIdentifier_("com.google.Chrome")
     if not chrome:
@@ -31,8 +38,9 @@ def reload_chrome():
     for window in chrome.windows():
         for tab in window.tabs():
             if sys.argv[2] in tab.URL():
-                print "Reloading {}".format(tab.URL())
+                print "Reloading Chrome: {}".format(tab.URL())
                 tab.reload()
+browser_reloaders['chrome'] = reload_chrome
 
 def reload_safari():
     safari = SBApplication.applicationWithBundleIdentifier_("com.apple.Safari")
@@ -42,27 +50,33 @@ def reload_safari():
     for window in safari.windows():
         for tab in window.tabs():
             if sys.argv[2] in tab.URL():
-                print "Reloading {}".format(tab.URL())
+                print "Reloading Safari: {}".format(tab.URL())
                 tab.reload()
+browser_reloaders['safari'] = reload_safari
     
 
-def event_callback(event):
+def event_callback(event, browsers):
     if "." not in event.name:
         return
     file_type = event.name.split(".")[-1].lower()
-        print "Detected change in {0}".format(event.name)
     if file_type in FILE_TYPES and any(event.mask & flag for flag in FLAGS):
+        print "Detected change in {}".format(event.name)
         if file_type in FILE_DELAYS:
             time.sleep(FILE_DELAYS[file_type])
-        reload_chrome()
+        for browser in browsers:
+            if browser in browser_reloaders:
+                browser_reloaders[browser]()
+            else:
+                print "Can't refresh unknown browser {}".format(browser)
 
 def main():
     if len(sys.argv) != 3:
         print "Usage: {} <path> <keyword>".format(sys.argv[0])
         sys.exit(1)
     print "Watching {} for changes...".format(os.path.abspath(sys.argv[1]))
+    event_callback_partial = partial(event_callback, browsers=("chrome",))
     observer = Observer()
-    stream = Stream(event_callback, sys.argv[1], file_events=True)
+    stream = Stream(event_callback_partial, sys.argv[1], file_events=True)
     observer.schedule(stream)
     observer.run()
 
